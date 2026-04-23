f85042f9ecbeb31c272a7b4e2f7473a2bfe057e5
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-23 11:18:43 +0700

    validasi data dan penyisipan di awal

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -27,5 +27,18 @@ public:
 
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
     }
 }
