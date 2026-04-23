de7ea0e3ac75036e5eb027ea8a89a5d0134b072f
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-23 11:44:00 +0700

    implementasi fungsi delNode

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -72,9 +72,25 @@ public:
         while (current != NULL && nim != current->noMhs)
         {
              previous = current;
-              current = current->next;
+             current = current->next;
         }
+        
+        return (current != NULL);
+    }
 
-      return (current != NULL);
+    bool delNode(int nim)
+    {  
+        Node *current, *previous;
+
+        if (!search(nim, previous, current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
     }
 }
