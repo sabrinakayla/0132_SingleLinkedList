5ef8b8e91cddd9df15c05e5d048ded367becb57b
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-23 11:53:25 +0700

    inisialisasi fungsi dan perulangan

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -114,3 +114,21 @@ public:
         }
     }
 };
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Menambah data ke dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+
+        cout << "\nMasukkan pilihan (1-5): ";
+        cin >> ch;
