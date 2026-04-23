3c0e6f900efbaf99ac649e3b4b6e602c04ecad69
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-23 11:33:59 +0700

    implementasi fungsi search

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -63,4 +63,18 @@ public:
     {
         return (START == NULL);
     }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+             previous = current;
+              current = current->next;
+        }
+
+      return (current != NULL);
+    }
 }
