9d71a82c576f11381a0e0c4104cc1ea9d2d595c4
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-23 12:19:24 +0700

    finalisasi

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -132,3 +132,63 @@ int main()
 
         cout << "\nMasukkan pilihan (1-5): ";
         cin >> ch;
+
+        switch (ch)
+        {
+            case '1':
+            mhs.addNode();
+            break;
+            
+            case '2':
+            if (mhs.listEmpty())
+            {
+                cout << "\nList Kosong" << endl;
+                break;
+            }
+
+            cout << "\nMasukkan no mahasiswa yang akan dihapus : ";
+            cin >> nim;
+
+            if (mhs.delNode(nim) == false)
+                cout << "\nData tidak ditemukan" << endl;
+            else
+                cout << "\nData dengan nomor mahasiswa " << nim << " berhasil dihapus\n";
+            break;
+            
+            case '3':
+            mhs.traverse();
+            break;
+
+            case '4':
+            {
+                if (mhs.listEmpty())
+                {
+                    cout << "\nList Kosong\n";
+                    break;
+                }
+
+                 Node *previous, *current;
+                 cout << "\nMasukkan no mahasiswa yang dicari : ";
+                 cin >> nim;
+
+                 if (mhs.search(nim, previous, current) == false)
+                    cout << "\nData tidak ditemukan\n";
+                 else
+                {
+                    cout << "\nData ditemukan\n";
+                    cout << "NIM Mahasiswa : " << current->noMhs << endl;
+                }
+                break;
+            }
+
+            case '5':
+                break;
+            
+            default:
+                cout << "\nPilihan salah!\n";
+        }
+
+    } while (ch != '5');
+
+    return 0;
+}
