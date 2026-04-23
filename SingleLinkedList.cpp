17420f35aa14ee80ec8ae87ea3bdcaf3c23d3a17
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-23 11:25:19 +0700

    melakukan perulangan

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -40,5 +40,19 @@ public:
             START = nodeBaru;
             return;
         }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                 return;
+            }
+            previous = current;
+            current = current->next;
+        }
     }
 }
